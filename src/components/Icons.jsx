7b22a0770b9e2490a44f4597aeaef09df9baf808
Addin Septyan <addinseptyan31@gmail.com>
2024-09-25 06:18:11 +0700

    update collapse icons

--- src/components/Icons.jsx
+++ src/components/Icons.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react'
 
 export const Icons = {
   ArrowDropDown: () => (
@@ -22,5 +22,27 @@ export const Icons = {
     >
       <path d='m280-400 200-200 200 200H280Z' />
     </svg>
-  )
-};
+  ),
+  KeyboardArrowDown: () => (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      height='24px'
+      viewBox='0 -960 960 960'
+      width='24px'
+      fill='currentColor'
+    >
+      <path d='M480-344 240-584l56-56 184 184 184-184 56 56-240 240Z' />
+    </svg>
+  ),
+  KeyboardArrowRight: () => (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      height='24px'
+      viewBox='0 -960 960 960'
+      width='24px'
+      fill='currentColor'
+    >
+      <path d='M504-480 320-664l56-56 240 240-240 240-56-56 184-184Z' />
+    </svg>
+  ),
+}
